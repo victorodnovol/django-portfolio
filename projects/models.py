3865f7de63d08a12afae4fc04b569d513c26a664
victorodnovol <victor.odnovol@gmail.com>
2023-04-27 20:42:27 +0300

    (style) add github link for project details page

--- projects/models.py
+++ projects/models.py
@@ -7,3 +7,4 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.CharField(max_length=100)
+    github = models.CharField(max_length=100)
