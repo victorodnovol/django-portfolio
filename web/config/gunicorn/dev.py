7e91f1224163220b1963cc761142dd0520f38b69
victorodnovol <victor.odnovol@gmail.com>
2023-06-12 10:36:41 +0300

    (fix) enable gunicorn to use tmpfs and prevent permanent hdd spin

--- web/config/gunicorn/dev.py
+++ web/config/gunicorn/dev.py
@@ -3,13 +3,13 @@
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
 wsgi_app = "portfolio.wsgi:application"
 # The granularity of Error log outputs
-loglevel = "debug"
+loglevel = "error"
 # The number of worker processes for handling requests
 workers = 2
 # The socket to bind
 bind = "0.0.0.0:8000"
 # Restart workers when code changes (development only!)
-reload = True
+# reload = True
 # Write access and error info to /var/log
 # accesslog = errorlog = "/var/log/gunicorn/dev.log"
 # Redirect stdout/stderr to log file
@@ -18,3 +18,5 @@ reload = True
 # pidfile = "/var/run/gunicorn/dev.pid"
 # Daemonize the Gunicorn process (detach & enter background)
 # daemon = True
+# Gunicorn workers use tmpfs to enable hdd spindown
+worker_tmp_dir = "/dev/shm"
